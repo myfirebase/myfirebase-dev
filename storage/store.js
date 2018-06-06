78357d7065282c8f5bb4545b0ef208e9897c750a
Amrani Houssain <houssain@airbit.com>
2018-06-06 02:26:57 +0100

    init storage

---
+++ storage/store.js
@@ -0,0 +1,42 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { firebase } from './../src/firebase/firebase'
+import auth from './../storage/modules/myfirebase-auth/auth'
+import storage from './../storage/modules/myfirebase-storage/storage'
+require('firebase/firestore')
+
+Vue.use(Vuex);
+
+// FCM, Firebase cloud messaging
+const fcm = firebase.messaging();
+// Cloud FireStore
+const firestore = firebase.firestore();
+firestore.settings({timestampsInSnapshots: true})
+// Defin database
+const database = firebase.database();
+// Ref to messages JSON
+const messages = database.ref().child('messages');
+// Error
+const error = ''
+
+export default new Vuex.Store({
+    // myfirebase modules
+    modules: {
+        auth: auth,
+        storage: storage
+    },
+    // states
+    state: {
+        database: database.ref(),
+        firestore: firestore,
+        error: error,
+        messaging: fcm,
+        firebase: firebase
+    },
+    // mutations
+    mutations: {},
+    // getters
+    getters: {},
+    //actions
+    actions: {}
+})
