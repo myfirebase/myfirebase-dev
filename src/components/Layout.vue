7c00a34123674b0880746fed827a0dc6e563a868
Amrani Houssain <houssain@airbit.com>
2018-06-09 06:21:50 +0100

    WIP Layout

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,16 +1,36 @@
 <template>
-    <div>
-      <v-app>
-        <v-navigation-drawer app></v-navigation-drawer>
-        <v-toolbar app></v-toolbar>
-        <v-content>
-          <v-container>
-            <router-view></router-view>
-          </v-container>
-        </v-content>
-        <v-footer app></v-footer>
-      </v-app>
-    </div>
+    <v-app dark>
+      <v-navigation-drawer app v-model="drawer" :permanent="drawer" hide-overlay width="200"
+    >
+        <v-list>
+          <v-list-tile v-for="item in items" :key="item.title">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar app>
+        <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Myfirebase</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>Docs</v-btn>
+          <v-btn flat>Github</v-btn>
+          <v-btn flat>Link Three</v-btn>
+          <v-btn flat>SignOut</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-content>
+        <v-container>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+      <v-footer app></v-footer>
+    </v-app>
 </template>
 <script>
 import navbar from "./../components/partials/Navbar";
@@ -18,14 +38,22 @@ export default {
   mounted() {
     this.$auth
       .check()
-      .then(user => {})
+      .then(user => {
+        this.loggedIn = true
+      })
       .catch(err => {});
   },
   data() {
     return {
       message: "Welcome Myfirebase SPA framework",
-      userLogged: false
-    };
+      loggedIn: false,
+      items: [
+        { title: 'Dashboard', icon: 'dashboard' },
+        { title: 'Account', icon: 'account_box' },
+        { title: 'Admin', icon: 'gavel' }
+      ],
+      drawer: true
+    }
   },
   methods: {},
   components: {
