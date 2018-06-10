c63f0d367192ba36664b8648f27cf474c2d368a7
Amrani Houssain <houssain@airbit.com>
2018-06-10 05:17:44 +0100

    WIP Layout

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,27 +1,34 @@
 <template>
     <v-app dark>
-      <v-navigation-drawer app v-model="drawer" :permanent="drawer" hide-overlay width="200"
-    >
+      <v-navigation-drawer app v-model="drawer" :permanent="drawer" hide-overlay width="200" v-if="$auth.user()">
+        <v-toolbar flat class="transparent">
+          <v-list class="pa-0">
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img src="https://randomuser.me/api/portraits/men/85.jpg" >
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>John Leider</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-toolbar>
         <v-list>
           <v-list-tile v-for="item in items" :key="item.title">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
             <v-list-tile-content>
               <v-list-tile-title>{{ item.title }}</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list>
       </v-navigation-drawer>
-      <v-toolbar app>
-        <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar app >
+        <v-toolbar-side-icon @click.native="drawer = !drawer" v-if="$auth.user()"></v-toolbar-side-icon>
         <v-toolbar-title>Myfirebase</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
           <v-btn flat>Docs</v-btn>
           <v-btn flat>Github</v-btn>
-          <v-btn flat>Link Three</v-btn>
-          <v-btn flat>SignOut</v-btn>
+          <v-btn flat v-if="$auth.user()">SignOut</v-btn>
         </v-toolbar-items>
       </v-toolbar>
       <v-content>
@@ -48,9 +55,8 @@ export default {
       message: "Welcome Myfirebase SPA framework",
       loggedIn: false,
       items: [
-        { title: 'Dashboard', icon: 'dashboard' },
-        { title: 'Account', icon: 'account_box' },
-        { title: 'Admin', icon: 'gavel' }
+        { title: 'Real time Database', link: 'dashboard' },
+        { title: 'Cloud Firestore', link: 'account_box' },
       ],
       drawer: true
     }
@@ -58,6 +64,9 @@ export default {
   methods: {},
   components: {
     navbar
+  }, 
+  watch: {
+
   }
 };
 </script>
